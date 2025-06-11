cc96129fc039926c09da9dbf814d5854363c8490
season91 <choayoung91@naver.com>
2025-06-11 11:05:42 +0900

    [chore] 예외처리 추가

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -7,5 +7,11 @@ public class ItemObject : MonoBehaviour
     private void Reset()
     {
         itemData = Resources.Load<ItemData>($"Item/Data/{name}");
+        
+        if (itemData == null)
+        {
+            Debug.LogWarning($"[ItemObject] 리소스 로드 실패: Item/Data/{name} 에 해당하는 ItemData가 없습니다.");
+            return;
+        }
     }
 }
